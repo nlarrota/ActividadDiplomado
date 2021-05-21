55964e750dbcb5147f7b302792231635c1ff817b
nlarrota <nlarrota@iberoamericana.edu.co>
2021-05-20 20:23:11 -0500

    Mi primer repositorio

---
+++ scrip.js
@@ -0,0 +1,117 @@
+
+
+$('#parrafo').css ("color","red")
+
+
+
+$(".pais1").click(function(){
+     $(this).css("background", "blue");
+     $(this).css ("width", "150px");
+})
+
+$(".pais1").mouseover (function(){
+    $(this).css("color","yellow");
+    $(this).css("font-size", "50px");
+})
+
+$(".pais2").mouseover (function(){
+    $(this).css("color","red");
+    $(this).css("font-size", "60px");
+})
+$(".pais2").dblclick(function(){
+    $(this).css("background", "pink");
+    $(this).css ("width", "250px");
+})
+
+$(".pais3").click(function(){
+    $(this).css("background", "green");
+    $(this).css ("width", "1000px");
+})
+$(".pais3").mouseover (function(){
+    $(this).css("color","blue");
+    $(this).css("font-size", "50px");
+})
+
+$(".pais4").mouseover (function(){
+   $(this).css("color","red");
+   $(this).css("font-size", "50px");
+})
+$(".pais4").click(function(){
+    $(this).css("background", "pink");
+    $(this).css ("width", "100px");
+})
+$(".pais5").click(function(){
+    $(this).css("background", "gray");
+    $(this).css ("width", "150px");
+})
+$(".pais5").mouseout (function(){
+    $(this).css("color","green");
+    $(this).css("font-size", "70px");
+})
+
+$(".pais6").mouseout (function(){
+   $(this).css("color","blue");
+   $(this).css("font-size", "60px");
+})
+$(".pais6").dblclick(function(){
+    $(this).css("background", "orange");
+    $(this).css ("width", "200px");
+})
+$(".pais7").click(function(){
+    $(this).css("background", "black");
+    $(this).css ("width", "200px");
+})
+$(".pais7").mousedown (function(){
+    $(this).css("color","purple");
+    $(this).css("font-size", "60px");
+ })
+
+$(".pais8").mouseover (function(){
+   $(this).css("color","red");
+   $(this).css("font-size", "50px");
+})
+$(".pais8").click(function(){
+    $(this).css("background", "brown");
+    $(this).css ("width", "200px");
+})
+$(".pais9").click(function(){
+    $(this).css("background", "blue");
+    $(this).css ("width", "50px");
+})
+
+$(".pais9").mouseout (function(){
+    $(this).css("color","purple");
+    $(this).css("font-size", "60px");
+ })
+
+$(".pais10").mouseover (function(){
+   $(this).css("color","red");
+   $(this).css("font-size", "30px");
+})
+
+$(".pais10").mouseout(function(){
+    $(this).css("background", "yellow");
+})
+$(".caja").mouseover(function(){
+    $(this).css("background", "pink");
+})
+
+$(".caja").mouseout(function(){
+    $(this).css("background", "white");
+})
+
+$(".caja2").mouseover(function(){
+    $(this).css("background", "yellow");
+})
+
+$(".caja2").mouseout(function(){
+    $(this).css("background", "white");
+})
+
+$(".caja3").mouseover(function(){
+    $(this).css("background", "purple");
+})
+
+$(".caja3").mouseout(function(){
+    $(this).css("background", "white");
+})
